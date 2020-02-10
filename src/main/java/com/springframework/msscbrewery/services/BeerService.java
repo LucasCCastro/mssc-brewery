d211d5c04cc1a8c4a472d95a7e7e51dfa752130b
lucas_c <lucas_c@dellteam.com>
2020-02-10 14:43:13 -0300

    Customer DTO, Controller and Service added

---
+++ src/main/java/com/springframework/msscbrewery/services/BeerService.java
@@ -0,0 +1,13 @@
+package com.springframework.msscbrewery.services;
+
+import com.springframework.msscbrewery.web.model.BeerDto;
+
+import java.util.UUID;
+
+public interface BeerService {
+    BeerDto getBeerById(UUID beerId);
+
+    BeerDto saveNewBeer(BeerDto beerDto);
+
+    void updateBeer(UUID beerId, BeerDto beerDto);
+}
--- src/main/java/com/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/com/springframework/msscbrewery/web/controller/BeerController.java
@@ -1,10 +1,11 @@
 package com.springframework.msscbrewery.web.controller;
 
+import com.springframework.msscbrewery.services.BeerService;
 import com.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -12,8 +13,33 @@ import java.util.UUID;
 @RestController
 public class BeerController {
 
-//    @GetMapping("/{beerId}")
-//    public ResponseEntity<BeerDto> getBeer(UUID) {
-//
-//    }
+    private final BeerService beerService;
+
+    public BeerController(BeerService beerService) {
+        this.beerService = beerService;
+    }
+
+    @GetMapping({"/{beerId}"})
+    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId){
+
+        return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody  BeerDto beerDto) {
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
 }
