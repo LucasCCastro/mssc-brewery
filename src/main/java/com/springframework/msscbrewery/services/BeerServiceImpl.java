ecfee62dd90736f8a68e0d8380adec3b7b8feed7
lucas_c <lucas_c@dellteam.com>
2020-02-12 14:41:46 -0300

    Project updated to V2

--- src/main/java/com/springframework/msscbrewery/services/BeerServiceImpl.java
+++ src/main/java/com/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.springframework.msscbrewery.services;
 
 import com.springframework.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -25,6 +27,11 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         //todo impl - would add a real impl to update beer
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer.");
     }
 
 
